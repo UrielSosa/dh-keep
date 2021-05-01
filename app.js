802c25d0b444e5d15d95f11df9cbe7fe7731de3b
Uriel Sosa <sosa.uriel1999@gmail.com>
2021-04-30 21:04:18 -0300

    creamos server y maquetado

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+/*Configuraciones*/
+app.use(express.static(path.resolve(__dirname,  'public')))
+
+/*Rutas*/
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+})
+
+
+/*Server*/
+app.listen(3000, () => console.log('http://localhost:3000'));
