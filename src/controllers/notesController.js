edfba1a17d57f229ec8fff49eef08a2e8369eb9b
Uriel Sosa <sosa.uriel1999@gmail.com>
2021-05-21 20:34:46 -0300

    new fixture agregar nota

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,10 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
-
 let notes = fs.readFileSync(path.resolve(__dirname, '../database/notes.json'), 'utf-8');
     notes = JSON.parse(notes);
 
+const lastId = () => {
+    let ultimo = 0;
+    notes.forEach(note => {
+        if (ultimo < note.id) {
+            ultimo = note.id;
+        }
+    });
+    return ultimo;
+}
 
 module.exports = {
     index: function (req, res) {
@@ -23,7 +31,22 @@ module.exports = {
         res.render('notes/create');
     },
     store: function (req, res) {
-        res.send(req.body);
+        //console.log(req.file); //acá llega la info de upload.single()
+        //console.log(req.files); //acá llega la info de upload.any()
+        
+        let note = {
+            id: lastId() + 1,
+            ...req.body,
+            image: req.file.filename
+        }
+
+        notes.push(note);
+
+        let notesJson = JSON.stringify(notes, null, 4);
+        
+        fs.writeFileSync(path.resolve(__dirname, '../database/notes.json'), notesJson);
+
+        return res.redirect('/notes');
     },
     destroy: function (req, res) {
         res.send('Logica de eliminar');
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const path = require('path');
+const multer = require('multer');
+
 const router = express.Router();
 const controller = require('../controllers/notesController');
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(__dirname, '../../public/img/notes'))
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+  })
+   
+  var upload = multer({ storage: storage })
+
+
 
 router.get('/', controller.index);
 router.get('/create', controller.create); //vista
@@ -8,7 +25,9 @@ router.get('/:id', controller.show);
 router.get('/:id/edit', controller.edit); //vista
 
 router.put('/:id', controller.update); //logica
-router.post('/', controller.store); //logica
+
+router.post('/', upload.single('image'), controller.store); //Acá agregamos multer
+
 router.delete('/:id', controller.destroy); //logica
 
 
